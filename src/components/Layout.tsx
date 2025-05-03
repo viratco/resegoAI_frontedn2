d37db405de869a455ab4f6b713596d8cee85bbe2
viratco <meenukumarpk@gmail.com>
2025-05-03 15:40:54 +0530

    more

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,19 @@
 import Navbar from './Navbar'
 import { Outlet } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 
 export function Layout() {
   return (
-    <div className="min-h-screen">
-      <Navbar />
-      <main className="container mx-auto px-4 pt-16">
-        <Outlet />
-      </main>
-    </div>
+    <>
+      <Helmet>
+        <meta name="google-site-verification" content="wC1CD66tpBTIXJ5t5IjIXycNO9xcopzmkklRePgQq4k" />
+      </Helmet>
+      <div className="min-h-screen">
+        <Navbar />
+        <main className="container mx-auto px-4 pt-16">
+          <Outlet />
+        </main>
+      </div>
+    </>
   )
 } 
